294e713602026a853d307cd61a53a1de073c49d5
Nguyen Thanh Nam <namtpd@gmail.com>
2020-09-07 11:18:05 -0500

    new update

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -3,5 +3,20 @@ class Admin::ReportsController < Admin::BaseController
   end
 
   def revenue
+    
+    if params[:fromDate] == "" || params[:fromDate] == nil
+      session[:fromdate] = "01/01/1900"
+    else
+      session[:fromdate] = params[:fromDate]
+    end
+    if params[:toDate] == "" || params[:toDate] == nil
+      session[:todate] = "01/01/2039"
+    else
+      session[:todate] = params[:toDate]
+    end
+    #@checkoutrecord = CheckoutRecord.all.where(pickupDate: [DateTime.now..(DateTime.now - 30.days)]).where.not(rentPrice: nil)
+    #@checkoutrecord = CheckoutRecord.all.where(pickupDate: [DateTime.parse(session[:fromdate]).beginning_of_day..DateTime.parse(session[:todate]).end_of_day]).where.not(rentPrice: nil)
+    @checkoutrecord = CheckoutRecord.all.where.not(rentPrice: nil)
+
   end
 end
